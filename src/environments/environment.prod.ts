631dbb1ff2a407f5088992556c1fdbb00e6251d6
Salvador Nicolas <salvador.pereiro@inetum.com>
2022-12-21 10:08:51 +0100

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+declare const window: any;
+
+// @see: https://pumpingco.de/blog/environment-variables-angular-docker/
+export const environment = {
+  production: true,
+  siteName: window.env?.siteName || 'angular-boilerplate',
+  apiUrl: window.env?.apiUrl || 'https://localhost:7001',
+  siteUrl: window.env?.siteUrl || 'http://localhost:4200',
+  apiSegment: window.env?.apiSegment || 'api/v1',
+  culture: window.env?.culture || 'es',
+};
