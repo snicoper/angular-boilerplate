6a829550fd2033823dc406b9b8565b4b06a922be
Salvador Nicolas <salvador.pereiro@inetum.com>
2022-12-21 13:37:26 +0100

    Generado componente sidebar

--- src/app/components/views/views.module.ts
+++ src/app/components/views/views.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NavbarModule } from './../navbar/navbar.module';
+import { SidebarModule } from './../sidebar/sidebar.module';
 import { ViewBaseComponent } from './view-base/view-base.component';
 import { ViewTitleComponent } from './view-title/view-title.component';
 
 @NgModule({
   declarations: [ViewBaseComponent, ViewTitleComponent],
   exports: [ViewBaseComponent],
-  imports: [CommonModule, NavbarModule]
+  imports: [CommonModule, NavbarModule, SidebarModule]
 })
 export class ViewsModule {}
