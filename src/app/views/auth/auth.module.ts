aa5479203570423cab29b7d09031aaef62d3e61c
Salvador Nicolas <salvador.pereiro@inetum.com>
2022-12-22 15:13:59 +0100

    Formulario de identificación

--- src/app/views/auth/auth.module.ts
+++ src/app/views/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ViewsModule } from './../../components/views/views.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, AuthRoutingModule, ViewsModule]
+  imports: [CommonModule, ReactiveFormsModule, AuthRoutingModule, ViewsModule]
 })
 export class AuthModule {}
--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { LayoutService } from './../../../services/layout.service';
 
 @Component({
@@ -7,11 +8,16 @@ import { LayoutService } from './../../../services/layout.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnDestroy {
+  form: FormGroup;
+
   private navbarValue = false;
   private sidebarValue = false;
   private footerValue = false;
 
-  constructor(private layoutService: LayoutService) {
+  constructor(private layoutService: LayoutService, private fb: FormBuilder) {
+    this.form = this.fb.group({});
+    this.buildForm();
+
     // Save current values.
     this.navbarValue = this.layoutService.showSidebarValue;
     this.sidebarValue = this.layoutService.showSidebarValue;
@@ -27,5 +33,18 @@ export class LoginComponent implements OnDestroy {
     this.layoutService.setValueNavbar(this.navbarValue);
     this.layoutService.setValueSidebar(this.sidebarValue);
     this.layoutService.setValueFooter(this.footerValue);
+  }
+
+  public handleSubmit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+  }
+
+  private buildForm(): void {
+    this.form = this.fb.group({
+      userName: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
   }
 }
