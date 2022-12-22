7dc24f80fc376b2460906a47c8b5155d83c53be1
Salvador Nicolas <salvador.pereiro@inetum.com>
2022-12-22 13:37:05 +0100

    Cambiado scope de SidebarService

--- src/app/components/sidebar/sidebar.module.ts
+++ src/app/components/sidebar/sidebar.module.ts
@@ -2,10 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SidebarComponent } from './sidebar.component';
+import { SidebarService } from './sidebar.service';
 
 @NgModule({
   declarations: [SidebarComponent],
   imports: [CommonModule, RouterModule],
-  exports: [SidebarComponent]
+  exports: [SidebarComponent],
+  providers: [SidebarService]
 })
 export class SidebarModule {}
