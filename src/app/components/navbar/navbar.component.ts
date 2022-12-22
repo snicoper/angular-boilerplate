8b91f3c236f3e63eafa158a1056ad170e6d1c24f
Salvador Nicolas <salvador.pereiro@inetum.com>
2022-12-22 12:15:03 +0100

    Configurado sidebar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { appEnvironments } from '../../core/config/_index';
+import { SidebarService } from './../sidebar/sidebar.service';
 
 @Component({
   selector: 'aw-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  public siteName = appEnvironments.siteName;
+export class NavbarComponent implements OnDestroy {
+  sidebarState: boolean;
+  siteName = appEnvironments.siteName;
+
+  private destroy$ = new Subject<void>();
+
+  constructor(private sidebarService: SidebarService) {
+    this.sidebarState = this.sidebarService.sidebarStateValue;
+
+    this.eventListener();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  toggleSidebarState(): void {
+    this.sidebarService.toggle();
+  }
+
+  private eventListener(): void {
+    this.sidebarService.sidebarState.pipe(takeUntil(this.destroy$)).subscribe({
+      next: (result: boolean) => (this.sidebarState = result)
+    });
+  }
 }
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { SidebarService } from './sidebar.service';
 
 @Component({
   selector: 'aw-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {}
+export class SidebarComponent implements OnDestroy {
+  sidebarState: boolean;
+
+  private destroy$ = new Subject<void>();
+
+  constructor(private sidebarService: SidebarService) {
+    this.sidebarState = sidebarService.sidebarStateValue;
+
+    this.eventListener();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  handleClick(): void {
+    this.sidebarService.toggle();
+  }
+
+  private eventListener(): void {
+    this.sidebarService.sidebarState.pipe(takeUntil(this.destroy$)).subscribe({
+      next: (result: boolean) => (this.sidebarState = result)
+    });
+  }
+}
